ec3813c71d4d053fd383309e38801e84d02deedf
Marcus Carvalho <marcuskbra@hotmail.com>
2015-10-05 21:07:52 -0300

    Image optimization

--- WebContent/js/scripts.js
+++ WebContent/js/scripts.js
@@ -1,3 +1,55 @@
+/***************** Mustache Templates for gallery and Modals ***************** */
+$(function () {
+	
+	var template = $('#galeria-template').html();
+	
+	if(template) {
+		
+		var slidersLiquida = new Array();
+		
+		slidersLiquida.push(galleryItem('Sopa de Carne com Lentilha', 'img/liquida/sopa-carne-lentilha-1.jpg', 'img/liquida/sopa-carne-lentilha-1.jpg,img/liquida/sopa-carne-lentilha-2.jpg', 'Deliciosa Sopa de Carne com Lentilha'));
+		slidersLiquida.push(galleryItem('Caldo de Frango', 'img/liquida/caldo-frango-1.jpg', 'img/liquida/caldo-frango-1.jpg,img/liquida/caldo-frango-2.jpg', 'Delicioso Caldo de Frango'));
+		slidersLiquida.push(galleryItem('Sopa de Frango com Feijão', 'img/liquida/sopa-frango-feijao-1.jpg', 'img/liquida/sopa-frango-feijao-1.jpg,img/liquida/sopa-frango-feijao-2.jpg', 'Deliciosa Sopa de Frango com Feijão'));
+		
+		var rendered = Mustache.render(template, {'produtos': slidersLiquida, 'cssClass': 'dark-box'});
+		$('#produtos-fase-liquida').html(rendered);
+		
+		var slidersPastosa = new Array();
+		
+		slidersPastosa.push(galleryItem('Estrogonofe de Carne', 'img/pastosa/estrogonofe-carne-1.jpg', 'img/pastosa/estrogonofe-carne-1.jpg,img/pastosa/estrogonofe-carne-2.jpg,img/pastosa/estrogonofe-carne-3.jpg', 'Delicioso Estrogonofe carne da fase pastosa'));
+		slidersPastosa.push(galleryItem('Risoto de bacalhau', 'img/pastosa/risoto-bacalhau-1.jpg', 'img/pastosa/risoto-bacalhau-1.jpg,img/pastosa/risoto-bacalhau-2.jpg', 'Delicioso Risoto bacalhau da fase pastosa'));
+		slidersPastosa.push(galleryItem('Escondidinho de Carne', 'img/pastosa/escondidinho-carne-1.jpg', 'img/pastosa/escondidinho-carne-1.jpg,img/pastosa/escondidinho-carne-2.jpg', 'Delicioso Escondidinho carne da fase pastosa'));
+		slidersPastosa.push(galleryItem('Lasanha de Carne com beringela', 'img/pastosa/lasanha-carne-beringela-1.jpg', 'img/pastosa/lasanha-carne-beringela-1.jpg,img/pastosa/lasanha-carne-beringela-2.jpg,img/pastosa/lasanha-carne-beringela-3.jpg,img/pastosa/lasanha-carne-beringela-4.jpg', 'Delicioso Lasanha carne beringela da fase pastosa'));
+		slidersPastosa.push(galleryItem('Bobó de Frango', 'img/pastosa/bobo-frango-1.jpg', 'img/pastosa/bobo-frango-1.jpg,img/pastosa/bobo-frango-2.jpg,img/pastosa/bobo-frango-3.jpg', 'Delicioso Bobo frango da fase pastosa'));
+		slidersPastosa.push(galleryItem('Lasanha de Frango com abobrinha', 'img/pastosa/lasanha-frango-abobrinha-1.jpg', 'img/pastosa/lasanha-frango-abobrinha-1.jpg,img/pastosa/lasanha-frango-abobrinha-2.jpg,img/pastosa/lasanha-frango-abobrinha-3.jpg', 'Delicioso Lasanha frango abobrinha da fase pastosa'));
+		slidersPastosa.push(galleryItem('Escondidinho de Frango', 'img/pastosa/escondidinho-frango-1.jpg', 'img/pastosa/escondidinho-frango-1.jpg,img/pastosa/escondidinho-frango-2.jpg', 'Delicioso Escondidinho frango da fase pastosa'));
+		slidersPastosa.push(galleryItem('Risoto de Frango', 'img/pastosa/risoto-frango-1.jpg', 'img/pastosa/risoto-frango-1.jpg,img/pastosa/risoto-frango-2.jpg,img/pastosa/risoto-frango-3.jpg', 'Delicioso Risoto frango da fase pastosa'));
+		slidersPastosa.push(galleryItem('Risoto de Carne', 'img/pastosa/risoto-carne-1.jpg', 'img/pastosa/risoto-carne-1.jpg,img/pastosa/risoto-carne-2.jpg,img/pastosa/risoto-carne-3.jpg', 'Delicioso Risoto carne da fase pastosa'));
+		
+		
+		var rendered = Mustache.render(template, {'produtos': slidersPastosa, 'cssClass': 'light-box'});
+		$('#produtos-fase-pastosa').html(rendered);
+		
+		var slidersFit = new Array();
+		
+		slidersFit.push(galleryItem('Barra de Cereais', 'img/fit/barra-cereais-1.jpg', 'img/fit/barra-cereais-1.jpg', ''));
+		slidersFit.push(galleryItem('Cookie de castanha de caju', 'img/fit/cookie-castanha-caju-1.jpg', 'img/fit/cookie-castanha-caju-1.jpg,img/fit/cookie-castanha-caju-2.jpg', ''));
+		slidersFit.push(galleryItem('Brigadeiro funcional', 'img/fit/brigadeiro-funcional-1.jpg', 'img/fit/brigadeiro-funcional-1.jpg', ''));
+
+		var rendered = Mustache.render(template, {'produtos': slidersFit, 'cssClass': 'light-box'});
+		$('#produtos-linha-fit').html(rendered);
+		
+		function galleryItem(title, image, modalImages, description) {
+			return { 
+				'title': title,
+				'image': image,
+				'modal-images': modalImages,
+				'description': description
+			};
+		}
+	}
+});
+
 /************ Collapse Mobile Menu *************/
 $(document).ready(function () {
     $(".navbar-nav li a").click(function (event) {
@@ -99,104 +151,75 @@ $(document).ready(function () {
 
 $(window).load(function () {
     if (jQuery().flexslider) {
-        $('.slider').flexslider({
-            animation: "slide",
-            directionNav: false,
-            controlNav: true,
-            touch: false,
-            pauseOnHover: true,
-            start: function () {
-                $.waypoints('refresh');
-            }
-        });
+        var $slider=$('.slider');
+        if ($slider) {
+        	$slider.flexslider({
+        		animation: "slide",
+        		directionNav: false,
+        		controlNav: true,
+        		touch: false,
+        		pauseOnHover: true,
+        		start: function () {
+        			$.waypoints('refresh');
+        		}
+        	});
+        }
     }
 });
 
 /***************** Modal Carousel ******************/
 $(function () {
-    $('a.expand[data-modal-images]').click(function () {
-        $('.carousel-inner').html('');
-        var dataModalImgs = $(this).data('modalImages');
-        var modalImgs = [];
-        if (dataModalImgs == null || dataModalImgs == undefined) {
-            modalImgs.push($(this).parent().parent().find('img').attr('src'));
-        } else {
-            modalImgs = dataModalImgs.split(',');
-        }
-        $.each(modalImgs, function (index, el) {
-            var div=$('<div/>').appendTo($('.carousel-inner')).addClass('item');
-            if (index == 0 ) {
-                div.addClass('active');
-                $('<img/>').appendTo(div).attr('src', el).addClass('img-responsive');
-            } else {
-                $('<img/>').appendTo(div).attr('data-src', el).addClass('img-responsive');
-            }
-        });
-            
-        
-        $('#lazycarouselmodal').modal();
-    });
+    var $galleryItem=$('a.expand[data-modal-images]');
+    if ($galleryItem) {
+    	$galleryItem.click(function () {
+    		$('.carousel-inner').html('');
+    		var dataModalImgs = $(this).data('modalImages');
+    		var modalImgs = [];
+    		if (dataModalImgs == null || dataModalImgs == undefined) {
+    			modalImgs.push($(this).parent().parent().find('img').attr('src'));
+    		} else {
+    			modalImgs = dataModalImgs.split(',');
+    		}
+    		$.each(modalImgs, function (index, el) {
+    			var div=$('<div/>').appendTo($('.carousel-inner')).addClass('item');
+    			if (index == 0 ) {
+    				div.addClass('active');
+    				$('<img/>').appendTo(div).attr('src', el).addClass('img-responsive');
+    			} else {
+    				$('<img/>').appendTo(div).attr('data-src', el).addClass('img-responsive');
+    			}
+    		});
+    		
+    		
+    		$('#lazycarouselmodal').modal();
+    	});
+    }
 });
 
 $(function () {
-    $('div.modal').on('show.bs.modal', function (e) {
-        var carousel = $(this).find('.carousel').hide(),
-            deferreds = [],
-            imgs = $('.carousel', this).find('img');
-        
-        // loop over each img
-        imgs.each(function () {
-            var self = $(this),
-                datasrc = self.attr('data-src');
-            
-            if (datasrc) {
-                var d = $.Deferred();
-                self.one('load', d.resolve).attr("src", datasrc).attr('data-src', '');
-                deferreds.push(d.promise());
-            }
-        });
-
-        $.when.apply($, deferreds).done(function () {
-            $('#ajaxloader').hide();
-            carousel.fadeIn(1000);
-        });
-    });
+    var $divModal=$('div.modal');
+    if ($divModal) {
+    	$divModal.on('show.bs.modal', function (e) {
+    		var carousel = $(this).find('.carousel').hide(),
+    		deferreds = [],
+    		imgs = $('.carousel', this).find('img');
+    		
+    		// loop over each img
+    		imgs.each(function () {
+    			var self = $(this),
+    			datasrc = self.attr('data-src');
+    			
+    			if (datasrc) {
+    				var d = $.Deferred();
+    				self.one('load', d.resolve).attr("src", datasrc).attr('data-src', '');
+    				deferreds.push(d.promise());
+    			}
+    		});
+    		
+    		$.when.apply($, deferreds).done(function () {
+    			$('#ajaxloader').hide();
+    			carousel.fadeIn(1000);
+    		});
+    	});
+    }
 });
-
-
-/***************** Mustache Templates for Modals ***************** */
-
-var template = $('#galeria-template').html();
-var slidersLiquida = new Array();
-
-slidersLiquida.push(galleryItem('Sopa de Carne com Lentilha', 'img/liquida/sopa-carne-lentilha-1.jpg', 'img/liquida/sopa-carne-lentilha-1.jpg,img/liquida/sopa-carne-lentilha-2.jpg', 'Deliciosa Sopa de Carne com Lentilha'));
-slidersLiquida.push(galleryItem('Caldo de Frango', 'img/liquida/caldo-frango-1.jpg', 'img/liquida/caldo-frango-1.jpg,img/liquida/caldo-frango-2.jpg', 'Delicioso Caldo de Frango'));
-slidersLiquida.push(galleryItem('Sopa de Frango com Feijão', 'img/liquida/sopa-frango-feijao-1.jpg', 'img/liquida/sopa-frango-feijao-1.jpg,img/liquida/sopa-frango-feijao-2.jpg', 'Deliciosa Sopa de Frango com Feijão'));
-
-var rendered = Mustache.render(template, {'produtos': slidersLiquida});
-$('#produtos-fase-liquida').html(rendered);
-
-var slidersPastosa = new Array();
-
-slidersPastosa.push(galleryItem('Estrogonofe de Carne ', 'img/pastosa/estrogonofe-carne-1.jpg', 'img/pastosa/estrogonofe-carne-1.jpg,img/pastosa/estrogonofe-carne-2.jpg,img/pastosa/estrogonofe-carne-3.jpg', 'Delicioso Estrogonofe carne da fase pastosa'));
-slidersPastosa.push(galleryItem('Risoto de bacalhau ', 'img/pastosa/risoto-bacalhau-1.jpg', 'img/pastosa/risoto-bacalhau-1.jpg,img/pastosa/risoto-bacalhau-2.jpg', 'Delicioso Risoto bacalhau da fase pastosa'));
-slidersPastosa.push(galleryItem('Escondidinho de Carne ', 'img/pastosa/escondidinho-carne-1.jpg', 'img/pastosa/escondidinho-carne-1.jpg,img/pastosa/escondidinho-carne-2.jpg', 'Delicioso Escondidinho carne da fase pastosa'));
-slidersPastosa.push(galleryItem('Lasanha de Carne e beringela ', 'img/pastosa/lasanha-carne-beringela-1.jpg', 'img/pastosa/lasanha-carne-beringela-1.jpg,img/pastosa/lasanha-carne-beringela-2.jpg,img/pastosa/lasanha-carne-beringela-3.jpg,img/pastosa/lasanha-carne-beringela-4.jpg', 'Delicioso Lasanha carne beringela da fase pastosa'));
-slidersPastosa.push(galleryItem('Bobó de Frango ', 'img/pastosa/bobo-frango-1.jpg', 'img/pastosa/bobo-frango-1.jpg,img/pastosa/bobo-frango-2.jpg,img/pastosa/bobo-frango-3.jpg', 'Delicioso Bobo frango da fase pastosa'));
-slidersPastosa.push(galleryItem('Lasanha de Frango e abobrinha ', 'img/pastosa/lasanha-frango-abobrinha-1.jpg', 'img/pastosa/lasanha-frango-abobrinha-1.jpg,img/pastosa/lasanha-frango-abobrinha-2.jpg,img/pastosa/lasanha-frango-abobrinha-3.jpg', 'Delicioso Lasanha frango abobrinha da fase pastosa'));
-slidersPastosa.push(galleryItem('Escondidinho de Frango ', 'img/pastosa/escondidinho-frango-1.jpg', 'img/pastosa/escondidinho-frango-1.jpg,img/pastosa/escondidinho-frango-2.jpg', 'Delicioso Escondidinho frango da fase pastosa'));
-slidersPastosa.push(galleryItem('Risoto de Frango ', 'img/pastosa/risoto-frango-1.jpg', 'img/pastosa/risoto-frango-1.jpg,img/pastosa/risoto-frango-2.jpg,img/pastosa/risoto-frango-3.jpg', 'Delicioso Risoto frango da fase pastosa'));
-slidersPastosa.push(galleryItem('Risoto de Carne ', 'img/pastosa/risoto-carne-1.jpg', 'img/pastosa/risoto-carne-1.jpg,img/pastosa/risoto-carne-2.jpg,img/pastosa/risoto-carne-3.jpg', 'Delicioso Risoto carne da fase pastosa'));
-
-var rendered = Mustache.render(template, {'produtos': slidersPastosa});
-$('#produtos-fase-pastosa').html(rendered);
-
-
-function galleryItem(title, image, modalImages, description) {
-	return { 
-		'title': title,
-		'image': image,
-		'modal-images': modalImages,
-		'description': description
-	};
-}
